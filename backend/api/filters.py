30ccc1c5d85f9de5c35ec1eff4237c96c1312558
Никита Пискунов <nikpna@yandex.ru>
2023-09-26 15:46:15 +0300

    фильтры

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -19,20 +19,20 @@ class RecipeFilter(FilterSet):
         to_field_name='slug',
     )
     is_favorited = filters.BooleanFilter(method='get_is_favorited')
-    is_in_shopping_cart = filters.BooleanFilter(
+    is_cart = filters.BooleanFilter(
         method='get_is_cart'
     )
 
     class Meta:
         model = Recipe
-        fields = ('__all__')
+        fields = ('tags', 'author', 'is_favorited', 'is_in_shopping_cart')
 
     def get_is_favorited(self, queryset, name, value):
         if value:
-            return queryset.filter(favoriting__user=self.request.user)
+            return queryset.filter(recipe_in_favorites__user=self.request.user)
         return queryset
 
     def get_is_cart(self, queryset, name, value):
         if value:
-            return queryset.filter(cart__user=self.request.user)
+            return queryset.filter(item_in_carts__user=self.request.user)
         return queryset
