4909a4ca0d9211bbe66f5754a365ab1ea5d70217
Никита Пискунов <nikpna@yandex.ru>
2023-09-28 17:20:47 +0300

    fixies

--- backend/api/views.py
+++ backend/api/views.py
@@ -23,6 +23,7 @@ class TagViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
     permission_classes = (AllowAny,)
+    pagination_class = None
 
 
 class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
