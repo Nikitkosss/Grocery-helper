a0c3a5e833dc32e9b417e9b4bab5b433e953e567
Никита Пискунов <nikpna@yandex.ru>
2023-09-29 10:56:05 +0300

    fixies

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -11,29 +11,39 @@ class IngredientFilter(FilterSet):
         fields = ('name',)
 
 
+IN_NOT_IN = (
+    (0, 'Not_In'),
+    (1, 'In'),
+)
+
+
 class RecipeFilter(FilterSet):
-    author = filters.ModelChoiceFilter(queryset=User.objects.all())
+
+    author = filters.ModelChoiceFilter(
+        queryset=User.objects.all()
+    )
+    is_in_shopping_cart = filters.ChoiceFilter(
+        choices=IN_NOT_IN, method='get_is_in'
+    )
+    is_favorited = filters.ChoiceFilter(
+        choices=IN_NOT_IN,
+        method='get_is_in'
+    )
     tags = filters.AllValuesMultipleFilter(
         field_name='tags__slug',
         label='Ссылка'
     )
-    is_favorited = filters.BooleanFilter(method='get_is_favorited')
-    is_cart = filters.BooleanFilter(
-        method='get_is_in_shopping_cart'
-    )
+
+    def get_is_in(self, queryset, name, value):
+        user = self.request.user
+        if user.is_authenticated:
+            if value == '1':
+                if name == 'is_favorited':
+                    queryset = queryset.filter(recipe_in_favorites__user=user)
+                if name == 'is_in_shopping_cart':
+                    queryset = queryset.filter(shopping_cart__user=user)
+        return queryset
 
     class Meta:
         model = Recipe
-        fields = ('tags', 'author', 'is_favorited', 'is_cart')
-
-    def get_is_favorited(self, queryset, name, value):
-        if value:
-            return queryset.filter(recipe_in_favorites__user=self.request.user)
-        return queryset
-
-    def get_is_in_shopping_cart(self, queryset, name, value):
-        if value:
-            return queryset.filter(
-                shopping_cart__user=self.request.user
-            )
-        return queryset
+        fields = ['is_favorited', 'is_in_shopping_cart', 'author', 'tags']
